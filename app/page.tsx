aca11f5b7a34ee709b913432e986799cf44822a5
bizrohee <brendanbirozy@gmail.com>
2026-01-18 15:35:38 -0800

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,10 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        Brendan Birozy
       </h1>
       <p className="mb-4">
-        {`I'm a 3rd year Electrical Engineering student at California State Polytechnic University, Pomona with a passion for schematic and PCB design, 
+        {`I am a 3rd year Electrical Engineering student at California State Polytechnic University, Pomona with a passion for schematic and PCB design, 
         especially for automotive and vehicular applications. I have hands-on experience designing and routing multi-layer boards for our 
         Formula SAE EV, including its custom Battery management System and accompanying modules using KiCAD.
         
