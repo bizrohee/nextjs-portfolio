b5aac18692da3978b2a9f391c4ddbfe9498a9dc6
bizrohee <brendanbirozy@gmail.com>
2026-01-18 15:44:56 -0800

    Update

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -74,10 +74,10 @@ export default function Blog({ params }) {
             image: post.metadata.image
               ? `${baseUrl}${post.metadata.image}`
               : `/og?title=${encodeURIComponent(post.metadata.title)}`,
-            url: `${baseUrl}/blog/${post.slug}`,
+            url: `${baseUrl}/projects/${post.slug}`,
             author: {
               '@type': 'Person',
-              name: 'My Portfolio',
+              name: 'Brendan Birozy',
             },
           }),
         }}
